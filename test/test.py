07b941cc338ff6e515f1dc1fcc648f157a8002d9
hinfeyg2 <hinfeyg2@gmail.com>
2017-02-24 23:22:13 +0000

    more tests

--- test/test.py
+++ test/test.py
@@ -5,6 +5,29 @@ from main import *
 
 class Test(unittest.TestCase):
 
+    def testParseFileMethodSmall(self):
+        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
+        self.assertTrue(len(self.a.parseFile()) == 4)
+
+    def testGetSizeSmall(self):
+        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
+        self.a.parseFile()
+        self.assertTrue(self.a.getSize() == 5)
+
+    def testGetParseLinesSmall(self):
+        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(len(self.a.getParseLines()) == 3)
+
+    def testGetResultSmall(self):
+        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 17)
+
     def testParseFileMethod(self):
         self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
         self.assertTrue(len(self.a.parseFile()) != 0)
