503d5b8e58bec7fdebb5083fd591efc800c4f8d7
hinfeyg2 <hinfeyg2@gmail.com>
2017-02-24 14:50:24 +0000

    getSize getParseLines methods and tests

--- src/main.py
+++ src/main.py
@@ -3,10 +3,57 @@ class LedSwitcher:
 
 	def __init__(self, filename):
 		self.filename = filename
+		self.parseList = []
+		self.switchSize = 0
+		self.ledStateList = []
 
 	def parseFile(self):
-		parseList = []
 		with open(self.filename) as f:
 			for line in f:
-				parseList.append(line)
-		return parseList
+				self.parseList.append(line.strip())
+		return self.parseList
+
+	def getSize(self):
+		if len(self.parseList) == 0:
+			return "List Empty"
+		else:
+			self.switchSize = int(self.parseList[0])
+			for i in range(0, self.switchSize):
+				for y in range(0, self.switchSize):
+					self.ledStateList.append([False, y])
+		return self.switchSize
+
+	def getParseLines(self):
+		self.parseList.pop(0)
+		for i in range(0, len(self.parseList)):
+			lineSplitSpaceList = self.parseList[i].split(" ")
+			if lineSplitSpaceList[0] == 'turn' and lineSplitSpaceList[1] == 'on':
+
+				xStart = lineSplitSpaceList[2].split(",")[0]
+				yStart = lineSplitSpaceList[2].split(",")[1]
+				xEnd = lineSplitSpaceList[4].split(",")[0]
+				yEnd = lineSplitSpaceList[4].split(",")[1]
+				self.parseList[i] = [True, xStart, yStart, xEnd, yEnd]
+
+			elif lineSplitSpaceList[0] == 'turn' and lineSplitSpaceList[1] == 'off':
+
+				xStart = lineSplitSpaceList[2].split(",")[0]
+				yStart = lineSplitSpaceList[2].split(",")[1]
+				xEnd = lineSplitSpaceList[4].split(",")[0]
+				yEnd = lineSplitSpaceList[4].split(",")[1]
+				self.parseList[i] = [False, xStart, yStart, xEnd, yEnd]
+
+			elif lineSplitSpaceList[0] == 'switch':
+				xStart = lineSplitSpaceList[1].split(",")[0]
+				yStart = lineSplitSpaceList[1].split(",")[1]
+				xEnd = lineSplitSpaceList[3].split(",")[0]
+				yEnd = lineSplitSpaceList[3].split(",")[1]
+				self.parseList[i] = [None, xStart, yStart, xEnd, yEnd]
+
+		# print(len(self.parseList))
+		return self.parseList
+
+# a = LedSwitcher("../test/testinputs/input_assign3.txt")
+# a.parseFile()
+# a.getSize()
+# print(a.getParseLines())
--- test/test.py
+++ test/test.py
@@ -9,5 +9,18 @@ class Test(unittest.TestCase):
         self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
         self.assertTrue(len(self.a.parseFile()) != 0)
 
+    def testGetSize(self):
+
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.assertTrue(self.a.getSize() == 1000)
+
+    def testGetParseLines(self):
+
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(len(self.a.getParseLines()) == 300)
+
 if __name__ == '__main__':
     unittest.main()
