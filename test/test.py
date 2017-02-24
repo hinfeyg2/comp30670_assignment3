d66aa20cc6ca4ba46c1a000979f434a247da6c6a
hinfeyg2 <hinfeyg2@gmail.com>
2017-02-24 22:45:38 +0000

    more tests

--- test/test.py
+++ test/test.py
@@ -10,17 +10,23 @@ class Test(unittest.TestCase):
         self.assertTrue(len(self.a.parseFile()) != 0)
 
     def testGetSize(self):
-
         self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
         self.a.parseFile()
         self.assertTrue(self.a.getSize() == 1000)
 
     def testGetParseLines(self):
-
         self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
         self.a.parseFile()
         self.a.getSize()
         self.assertTrue(len(self.a.getParseLines()) == 300)
+
+    def testGetResult(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 400410)
 
 if __name__ == '__main__':
     unittest.main()
