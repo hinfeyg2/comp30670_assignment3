663b905239a80dfc71573f88ee744690893b8082
hinfeyg2 <hinfeyg2@gmail.com>
2017-02-25 00:30:32 +0000

    added comments

--- src/main.py
+++ src/main.py
@@ -2,22 +2,34 @@ import matplotlib.pyplot as plt
 
 class LedSwitcher:
 
+
 	def __init__(self, filename):
+
 		self.filename = filename
+
+		# Will contain each line from the assigned input file.
 		self.parseList = []
+		# Will be set to how big the size of the LED array is.
 		self.switchSize = 0
+		# Will contain a list of each led and its associated state.
 		self.ledStateList = []
 
+
 	def parseFile(self):
+		"""This method opens the file containing the assigned data input.
+		It returns a list with each line in that file. It avoids empty lines."""
 		
 		with open(self.filename) as f:
 			for line in f:
 				if len(line) > 1:
 					self.parseList.append(line.strip())
-		print(len(self.parseList), "parseList")
+		f.closed
 		return self.parseList
 
+
 	def getSize(self):
+		"""This method gets the size of the current LED array."""
+		
 		if len(self.parseList) == 0:
 			return "List Empty"
 		else:
@@ -27,7 +39,10 @@ class LedSwitcher:
 					self.ledStateList.append([False, x, y])
 		return self.switchSize
 
+
 	def getParseLines(self):
+		"""take each line from the parse list and convert it to something readable.
+		Perhaps this could be merged with the parseFile method to reduce iteratsion."""
 		
 		self.parseList.pop(0)
 		for i in range(0, len(self.parseList)):
@@ -41,7 +56,6 @@ class LedSwitcher:
 				self.parseList[i] = [True, xStart, yStart, xEnd, yEnd]
 
 			elif lineSplitSpaceList[0] == 'turn' and lineSplitSpaceList[1] == 'off':
-
 				xStart = lineSplitSpaceList[2].split(",")[0]
 				yStart = lineSplitSpaceList[2].split(",")[1]
 				xEnd = lineSplitSpaceList[4].split(",")[0]
@@ -56,6 +70,3 @@ class LedSwitcher:
 				self.parseList[i] = [None, xStart, yStart, xEnd, yEnd]
 
 		return self.parseList
-
-
-
