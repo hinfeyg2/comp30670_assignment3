ce897e96bbe133179e1756e6f5e7ab62a898072c
hinfeyg2 <hinfeyg2@gmail.com>
2017-02-28 21:26:36 +0000

    uncomment tests

--- test/test.py
+++ test/test.py
@@ -8,60 +8,57 @@ class Test(unittest.TestCase):
 
     # Testing each method...
 
-    # def testParseFileMethodSmall(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-    #     self.assertTrue(len(self.a.parseFile()) == 1001)
+    def testParseFileMethodSmall(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
+        self.assertTrue(len(self.a.parseFile()) == 1001)
 
-    # def testGetSizeSmall(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-    #     self.a.parseFile()
-    #     self.assertTrue(self.a.getSize() == 1000)
+    def testGetSizeSmall(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
+        self.a.parseFile()
+        self.assertTrue(self.a.getSize() == 1000)
 
-    # def testGetParseLinesSmall(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.assertTrue(len(self.a.getParseLines()) == 1000)
+    def testGetParseLinesSmall(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(len(self.a.getParseLines()) == 1000)
 
-    # def testGetResultSmall(self):
-    #     self.a = LedSwitcher("../test/testinputs/testLength5.txt")
-    #     self.a.parseFile()
-    #     self.assertTrue(self.a.getResult() == 17)
+    def testGetResultSmall(self):
+        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 17)
 
-    # def testParseFileMethod(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.assertTrue(len(self.a.parseFile()) != 0)
+    def testParseFileMethod(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.assertTrue(len(self.a.parseFile()) != 0)
 
-    # def testGetSize(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.a.parseFile()
-    #     self.assertTrue(self.a.getSize() == 1000)
+    def testGetSize(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.assertTrue(self.a.getSize() == 1000)
 
-    # def testGetParseLines(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.assertTrue(len(self.a.getParseLines()) == 300)
+    def testGetParseLines(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(len(self.a.getParseLines()) == 300)
 
     # Testing parsing of each line...
 
-    # def testLineParsingNormal(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.assertTrue(self.a.parseEachLine("turn on 619,181 through 736,944") == [True, 619, 181, 736, 944])
+    def testLineParsingNormal(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(self.a.parseEachLine("turn on 619,181 through 736,944") == [True, 619, 181, 736, 944])
 
-    # def testLineParsingCommaSpace(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.assertTrue(self.a.parseEachLine("svitch 857,894 through 920 ,932") == [None, 857, 894, 920, 932])
-
-    # def testLineParsingFrontSpace(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.assertTrue(self.a.parseEachLine(" svitch 857,894 through 920,932") == [None, 857, 894, 920, 932])
+    def testLineParsingCommaSpace(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.assertTrue(self.a.parseEachLine("switch 857,894 through 920 ,932") == [None, 857, 894, 920, 932])
 
     # With supplied test files...
 
@@ -73,37 +70,37 @@ class Test(unittest.TestCase):
         self.a.applyValues()
         self.assertTrue(self.a.getResult() == 400410)
 
-    # def testGetResultAssign3a(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3a.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.a.getParseLines()
-    #     self.a.applyValues()
-    #     self.assertTrue(self.a.getResult() == 7)
+    def testGetResultAssign3a(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3a.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 7)
 
-    # def testGetResultAssign3b(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3b.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.a.getParseLines()
-    #     self.a.applyValues()
-    #     self.assertTrue(self.a.getResult() == 29942250)
+    def testGetResultAssign3b(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3b.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 29942250)
 
-    # def testGetResultAssign3c(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3c.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.a.getParseLines()
-    #     self.a.applyValues()
-    #     self.assertTrue(self.a.getResult() == 477452)
+    def testGetResultAssign3c(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3c.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 477452)
 
-    # def testGetResultAssign3d(self):
-    #     self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-    #     self.a.parseFile()
-    #     self.a.getSize()
-    #     self.a.getParseLines()
-    #     self.a.applyValues()
-    #     self.assertTrue(self.a.getResult() == 349037)
+    def testGetResultAssign3d(self):
+        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
+        self.a.parseFile()
+        self.a.getSize()
+        self.a.getParseLines()
+        self.a.applyValues()
+        self.assertTrue(self.a.getResult() == 349037)
 
 if __name__ == '__main__':
     unittest.main()
