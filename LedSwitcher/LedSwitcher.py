7e7677ca23313bc1d5edb7db4c860e4453d46f3e
hinfeyg2 <hinfeyg2@gmail.com>
2017-03-01 14:26:18 +0000

    Refactoring and Updated Tests

--- LedSwitcher/LedSwitcher.py
+++ LedSwitcher/LedSwitcher.py
@@ -6,54 +6,42 @@ class LedSwitcher:
 	def __init__(self, filename):
 
 		self.filename = filename
-
-		# Will contain each line from the assigned input file.
-		self.parseList = []
 		# Will be set to how big the size of the LED array is.
 		self.switchSize = 0
 		# Will contain a list of each led and its associated state.
 		self.ledStateList = []
 
+
 	def parseFile(self):
 		"""This method opens the file containing the assigned data input.
 		It returns a list with each line in that file or url. It avoids empty lines."""
-		
+		count = 0
 		if self.filename[:4] == "http":
 			for line in urlopen(self.filename):
 				line = line.decode("utf-8")
 				line = line.strip()
-				if len(line) > 1:
-					self.parseList.append(line)
+				if count == 0:
+					self.switchSize = int(line)
+					self.getSize()
+					count = count + 1
+				elif len(line) > 1:
+					self.applyValues(self.parseEachLine(line))
 		else:
 			with open(self.filename) as f:
+				self.switchSize = int(f.readline())
+				self.getSize()
 				for line in f:
 					if len(line) > 1:
-						self.parseList.append(line.strip())
+						self.applyValues(self.parseEachLine(line))
 			f.closed
-		return self.parseList
-
 
 	def getSize(self):
 		"""This method gets the size of the current LED array."""
 		
-		if len(self.parseList) == 0:
-			return "List Empty"
-		else:
-			self.switchSize = int(self.parseList[0])
-			for x in range(0, self.switchSize):
-				for y in range(0, self.switchSize):
-					self.ledStateList.append([False, x, y])
+		for x in range(0, self.switchSize):
+			for y in range(0, self.switchSize):
+				self.ledStateList.append([False])
 		return self.switchSize
-
-
-	def getParseLines(self):
-			"""take each line from the parse list and convert it to something readable.
-			Perhaps this could be merged with the parseFile method to reduce iteratsion."""
-			
-			self.parseList.pop(0)
-			for i in range(0, len(self.parseList)):
-				self.parseList[i] = self.parseEachLine(self.parseList[i])
-			return self.parseList
 
 
 	def parseEachLine(self, x):
@@ -105,37 +93,22 @@ class LedSwitcher:
 			self.ledStateList[Index][0] = boolean
 		else:
 			self.ledStateList[Index][0] = state
-		self.ledStateList[Index][1] = curX
-		self.ledStateList[Index][2] = curY
 
 
-	def applyValues(self):
+	def applyValues(self, lineItem):
 		"""Apply the valus in the parseList to the ledStateList"""
-			for i in self.parseList:
-				x = []
-				y = []
-				state = []
-				for a in range(i[1], i[3] + 1):
-					for b in range(i[2], i[4] + 1):
-						x.append(a)
-						y.append(b)
-						state.append(i[0])
+		for a in range(lineItem[1], lineItem[3] + 1):
+			for b in range(lineItem[2], lineItem[4] + 1):
+				self.changeState(a, b, lineItem[0])
 
-				for i in range(len(x)):
-					self.changeState(x[i],y[i],state[i])
-				# return [len(x), len(y), len(state)]
 
 	def getResult(self):
 		"""returns the result."""
 
-		x = []
-		y = []
 		counter = 0
-		for i, data in enumerate(self.ledStateList):
-			if data[0] == True:
+		for i in self.ledStateList:
+			if i[0] == True:
 				counter = counter + 1
-				x.append(data[1])
-				y.append(data[2])
 		return counter
 
 def main():
--- test/test.py
+++ test/test.py
@@ -7,77 +7,50 @@ class Test(unittest.TestCase):
 
     # Testing each method...
 
-    def testParseFileMethodSmall(self):
-        """Tests that the class is reading the correct number of lines in a file"""
-
-        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-        self.assertTrue(len(self.a.parseFile()) == 1001)
-
-    def testGetParseLines3d(self):
-        """Makes sure that the Parselines method is examining all lines in the file"""
-
-        self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
-        self.a.parseFile()
-        self.a.getSize()
-        self.assertTrue(len(self.a.getParseLines()) == 1000)
-
-    def testGetParseLines3(self):
-        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-        self.a.parseFile()
-        self.a.getSize()
-        self.assertTrue(len(self.a.getParseLines()) == 300)
-
     def testGetResultSmall(self):
         """Tests the whole program on a small custom data set."""
 
-        self.a = LedSwitcher("../test/testinputs/testLength5.txt")
-        self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
-        self.assertTrue(self.a.getResult() == 17)
-
-    def testParseFileMethod(self):
-        """Makes sure that parseFile is reading an input."""
-
-        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-        self.assertTrue(len(self.a.parseFile()) != 0)
+        a = LedSwitcher("../test/testinputs/testLength5.txt")
+        a.parseFile()
+        self.assertTrue(a.getResult() == 17)
 
     def testGetSize(self):
         """Asserts that the getSize method is working correctly"""
 
-        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-        self.a.parseFile()
-        self.assertTrue(self.a.getSize() == 1000)
+        a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        a.parseFile()
+        self.assertTrue(a.switchSize == 1000)
 
     # Testing parsing of each line...
 
     def testLineParsingNormal(self):
         """testing the line parsing output."""
 
-        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
-        self.a.parseFile()
-        self.a.getSize()
-        self.assertTrue(self.a.parseEachLine("turn on 619,181 through 736,944") == [True, 619, 181, 736, 944])
+        a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        a.parseFile()
+        self.assertTrue(a.parseEachLine("turn on 619,181 through 736,944") == [True, 619, 181, 736, 944])
 
     def testLineParsingCommaSpace(self):
         """testing the line parsing output."""
 
-        self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        a = LedSwitcher("../test/testinputs/input_assign3.txt")
+        a.parseFile()
+        self.assertTrue(a.parseEachLine("switch 857,894 through 920 ,932") == [None, 857, 894, 920, 932])
+
+    # # With supplied test files...
+
+    def testGetResultAssign3Web(self):
+        """Test result against input files from web."""
+
+        self.a = LedSwitcher("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.assertTrue(self.a.parseEachLine("switch 857,894 through 920 ,932") == [None, 857, 894, 920, 932])
+        self.assertTrue(self.a.getResult() == 400410)
 
-    # With supplied test files...
-
-    def testGetResultAssign3(self):
+    def testGetResultAssign3Local(self):
         """Test result against input files."""
 
         self.a = LedSwitcher("../test/testinputs/input_assign3.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
         self.assertTrue(self.a.getResult() == 400410)
 
     def testGetResultAssign3a(self):
@@ -85,9 +58,6 @@ class Test(unittest.TestCase):
 
         self.a = LedSwitcher("../test/testinputs/input_assign3a.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
         self.assertTrue(self.a.getResult() == 7)
 
     def testGetResultAssign3b(self):
@@ -95,9 +65,6 @@ class Test(unittest.TestCase):
 
         self.a = LedSwitcher("../test/testinputs/input_assign3b.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
         self.assertTrue(self.a.getResult() == 29942250)
 
     def testGetResultAssign3c(self):
@@ -105,9 +72,6 @@ class Test(unittest.TestCase):
 
         self.a = LedSwitcher("../test/testinputs/input_assign3c.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
         self.assertTrue(self.a.getResult() == 477452)
 
     def testGetResultAssign3d(self):
@@ -115,9 +79,6 @@ class Test(unittest.TestCase):
 
         self.a = LedSwitcher("../test/testinputs/input_assign3d.txt")
         self.a.parseFile()
-        self.a.getSize()
-        self.a.getParseLines()
-        self.a.applyValues()
         self.assertTrue(self.a.getResult() == 349037)
 
 if __name__ == '__main__':
