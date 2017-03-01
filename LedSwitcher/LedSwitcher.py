10641b8d45ebb36ae15d7635a5a47700af34069c
hinfeyg2 <hinfeyg2@gmail.com>
2017-03-01 19:04:19 +0000

    now implemented using a list instead of a set

--- LedSwitcher/LedSwitcher.py
+++ LedSwitcher/LedSwitcher.py
@@ -6,7 +6,7 @@ class LedSwitcher:
 	def __init__(self, filename):
 
 		self.filename = filename
-		self.setTest = set()
+		self.listTest = []
 		self.switchSize = 0
 		# self.dictionaryTest[curX,curY] = state
 
@@ -34,7 +34,7 @@ class LedSwitcher:
 						if self.parseEachLine(line) != True:
 							self.applyValues(self.parseEachLine(line))
 			f.closed
-		return len(self.dictionaryTest)
+		return len(self.listTest)
 
 	def parseEachLine(self, x):
 		"""this file parses the data from each line and returns a list with the results."""
@@ -76,18 +76,18 @@ class LedSwitcher:
 
 	def changeState(self, curX, curY, state):
 		"""a method for changing the state of the ledStateList with and an input position and state"""
-		tempTuple = (curX, curY)
+		tempTuple = [curX, curY]
 		if state is None:
-			if tempTuple in self.setTest:
-				self.setTest.remove(tempTuple)
+			if tempTuple in self.listTest:
+				self.listTest.remove(tempTuple)
 			else:
-				self.setTest.add(tempTuple)
+				self.listTest.append(tempTuple)
 		elif state:
-			if tempTuple not in self.setTest:
-				self.setTest.add(tempTuple)
+			if tempTuple not in self.listTest:
+				self.listTest.append(tempTuple)
 		else:
-			if tempTuple in self.setTest:
-				self.setTest.remove(tempTuple)
+			if tempTuple in self.listTest:
+				self.listTest.remove(tempTuple)
 
 	def applyValues(self, lineItem):
 		"""Apply the valus in the parseList to the ledStateList"""
